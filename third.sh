7532b34a0eca6f52c91c1e64199cd6ad2b3469b5
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-09-09 19:39:24 +0200

    Update third.sh

--- third.sh
+++ third.sh
@@ -3,8 +3,9 @@
 # by blbMS
 
 function screen_ls {
-        screen -ls | sed -E "s/Qubic/\x1b[1;31m&\x1b[0m/g; s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g; s/Watch/\x1b[33m&\x1b[0m/g; s/block_update/\x1b[1;35m&\x1b[0m/g" | tail -n +2 | head -n -1
+        screen -ls | sed -E "s/Third/\x1b[1;34m&\x1b[0m/g; s/Qubic/\x1b[1;31m&\x1b[0m/g; s/CCminer/\x1b[32m&\x1b[0m/g" | tail -n +2 | head -n -1
 }
+
 echo -e "\n\e[1;94mStarting Third miner QUBIC / VERUS \e[0m\n"
 screen -X -S Third quit
 screen -wipe 1>/dev/null 2>&1
