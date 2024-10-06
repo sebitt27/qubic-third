7fd4e8f5aff2eb218a7f026a4b534f571256efe8
sebitt27 <136931061+sebitt27@users.noreply.github.com>
2024-10-06 14:35:56 +0200

    Update third_miner.sh
    remote thirdstartA
    add specific aleo

--- third_miner.sh
+++ third_miner.sh
@@ -2,46 +2,27 @@
 # v.2024-09-09
 # by blbMS
 
-LOG_FILE="$HOME/apoolminer/qubic.log"
+LOG_FILE="/home/miner/apool2.3/apoolminer_linux_v2.3.0/qubic.log"
 INTERVAL=30             # in sec
 found_status=false
 last_status=""
 
 function screen_ls {
-    screen -ls | sed -E "s/Third/\x1b[1;34m&\x1b[0m/g; s/Qubic/\x1b[1;31m&\x1b[0m/g; s/CCminer/\x1b[32m&\x1b[0m/g" | tail -n +2 | head -n -1
+    screen -ls | sed -E "s/Third/\x1b[1;34m&\x1b[0m/g; s/Qubic/\x1b[1;31m&\x1b[0m/g; s/Aleo/\x1b[32m&\x1b[0m/g" | tail -n +2 | head -n -1
 }
 
 function third_start {
-    echo -e "\e[1;92mQUBIC = IDLE, starting VERUS\e[0m"
-    screen -X -S CCminer quit
-    screen -wipe 1>/dev/null 2>&1
-    screen -dmS CCminer 1>/dev/null 2>&1
-    VERUS="$HOME/apoolminer/ccminer -c $HOME/apoolminer/ccminer.json"
-    screen -S CCminer -X stuff "$VERUS\n" 1>/dev/null 2>&1
-    screen_ls
-    echo $(date)
-}
-
-function third_stop {
-    echo -e "\e[1;91mQUBIC = MINING, closing VERUS\e[0m"
-    screen -X -S CCminer quit
-    screen_ls
-    echo $(date)
-}
-
-# ALEO  - not working yet
-function third_start_A {
     echo -e "\e[1;92mQUBIC = IDLE, starting ALEO\e[0m"
     screen -X -S Aleo quit
     screen -wipe 1>/dev/null 2>&1
     screen -dmS Aleo 1>/dev/null 2>&1
-    ALEO="$HOME/apoolminer/apoolminer_aleo -A aleo --account CP_wxxxxxxx --pool aleo1.hk.apool.io:9090 --gpu-off --thread 20 --worker XX"
+    ALEO="$HOME/apoolminer/aleominer -u stratum+tcp://aleo-asia.f2pool.com:4400 -w sebit27"
     screen -S Aleo -X stuff "$ALEO\n" 1>/dev/null 2>&1
     screen_ls
     echo $(date)
 }
 
-function third_stop_A {
+function third_stop {
     echo -e "\e[1;91mQUBIC = MINING, closing ALEO\e[0m"
     screen -X -S Aleo quit
     screen_ls
@@ -82,8 +63,8 @@ while true; do
     done < <(tac "$LOG_FILE")
 
     if [[ "$current_status" == "idle" && "$last_status" != "idle" ]]; then
-        if screen -ls | grep -q "CCniner"; then
-            echo -e "\e[0;93mCCminer already running\e[0m"
+        if screen -ls | grep -q "Aleo"; then
+            echo -e "\e[0;93mAleo already running\e[0m"
         else
             third_start
             last_status="idle"
