2c6d170051bba6584f9c2edf96b9a568fdcc4ec1
sebitt27 <136931061+sebitt27@users.noreply.github.com>
2024-10-06 14:26:38 +0200

    Update third.sh
    switch aleo

--- third.sh
+++ third.sh
@@ -6,7 +6,7 @@ function screen_ls {
         screen -ls | sed -E "s/Third/\x1b[1;34m&\x1b[0m/g; s/Qubic/\x1b[1;31m&\x1b[0m/g; s/CCminer/\x1b[32m&\x1b[0m/g" | tail -n +2 | head -n -1
 }
 
-echo -e "\n\e[1;94mStarting Third miner QUBIC / VERUS \e[0m\n"
+echo -e "\n\e[1;94mStarting Third miner QUBIC / ALEO \e[0m\n"
 screen -X -S Third quit
 screen -wipe 1>/dev/null 2>&1
 screen -dmS Third 1>/dev/null 2>&1
